6ec979767dd71894b2eefbf5a0ec8574d6f5b9a7
Giuseppe Maria Sarda <giuseppe.sarda16@gmail.com>
2025-08-19 15:30:09 +0200

    fix tcl 1

--- sim/questa/sim-run.tcl
+++ sim/questa/sim-run.tcl
@@ -5,14 +5,14 @@
 # Author: Giuseppe Sarda <giuseppe.sarda@esat.kuleuven.be>
 # Basic build script for QuestaSim
 
-if {[info exists ::env(FORCE_BUILD)]} {
+if {[info exists ::env(FORCE_BUILD)]}{
     set FORCE_BUILD $::env(FORCE_BUILD)
 } else {
     set FORCE_BUILD 1
 }
 
 # If OBJ is not defined then search on the environment
-if {! [info exists OBJ]} {
+if {![info exists OBJ]}{
     if {[info exists ::env(OBJ)]} {
         set OBJ $::env(OBJ)
     } else {
@@ -38,12 +38,12 @@ set IterationLimit 200000
 
 # Run simulation
 vsim -quiet \
-     -wlf work/${SIM_NAME}.wlf \
-     -msgmode both -displaymsgmode both \
-     -L work_lib  \
-     -work ${WLIB} \
-     -modelsimini ./modelsim.ini \
-     ${OBJ}
+    -wlf work/${SIM_NAME}.wlf \
+    -msgmode both -displaymsgmode both \
+    -L work_lib  \
+    -work ${WLIB} \
+    -modelsimini ./modelsim.ini \
+    ${OBJ}
 
 run -all
 quit
