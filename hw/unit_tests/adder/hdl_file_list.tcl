02458ea5101f4a6c7fce3414186af7db8d653552
Giuseppe Maria Sarda <giuseppe.sarda16@gmail.com>
2025-08-19 14:28:05 +0200

    add license

--- hw/unit_tests/adder/hdl_file_list.tcl
+++ hw/unit_tests/adder/hdl_file_list.tcl
@@ -1,3 +1,7 @@
+# Copyright 2025 KU Leuven.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
 set HDL_PATH ../../rtl
 
 set HDL_FILES [ list \
--- hw/unit_tests/adder/tb_adder.sv
+++ hw/unit_tests/adder/tb_adder.sv
@@ -1,3 +1,7 @@
+// Copyright 2025 KU Leuven.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
 `timescale 1ns / 1ps
 
 module tb_adder;
