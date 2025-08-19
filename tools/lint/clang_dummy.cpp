89b12dc135bc3cc94bafcfdfbe5593c088f445f9
Giuseppe Maria Sarda <giuseppe.sarda16@gmail.com>
2025-08-19 13:27:15 +0200

    fix linting issues

--- tools/lint/clang_dummy.cpp
+++ tools/lint/clang_dummy.cpp
@@ -1,6 +1,10 @@
+// Copyright 2024 KU Leuven.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  std::cout << "Hello World!" << std::endl;
-  return 0;
+    std::cout << "Hello World!" << std::endl;
+    return 0;
 }
