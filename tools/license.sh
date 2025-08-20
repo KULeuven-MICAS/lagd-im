e33f610727843482128890b035b33a2f345da8f7
Giuseppe Maria Sarda <giuseppe.sarda16@gmail.com>
2025-08-20 10:48:35 +0200

    fix licensing

--- tools/license.sh
+++ tools/license.sh
@@ -1,1 +1,5 @@
+# Copyright 2025 KU Leuven.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
 source /esat/micas-data/data/design/scripts/questasim_2025.2.rc
--- tools/utils/get_hdl_flist.py
+++ tools/utils/get_hdl_flist.py
@@ -1,3 +1,7 @@
+# Copyright 2025 KU Leuven.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
 from parsers import ParserClass
 
 parser = ParserClass()
