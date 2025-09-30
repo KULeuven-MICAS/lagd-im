2c28dd373972e25ae91559d735eeb11b9a9b2de8
jiacong sun <jiacong.sun@esat.kuleuven.be>
2025-09-30 21:36:37 +0200

    add spin caching module into file list

--- hw/unit_tests/energy_monitor/hdl_file_list.tcl
+++ hw/unit_tests/energy_monitor/hdl_file_list.tcl
@@ -9,6 +9,7 @@ set HDL_FILES [ list \
     "${HDL_PATH}/energy_monitor.sv" \
     "${HDL_PATH}/lib/bp_pipe.sv" \
     "${HDL_PATH}/counter_ctrl.sv" \
+    "${HDL_PATH}/vector_caching.sv" \
     "${HDL_PATH}/step_counter.sv" \
     "${HDL_PATH}/logic_ctrl.sv" \
     "${HDL_PATH}/vector_mux.sv" \
