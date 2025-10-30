ca01e5f5324d50eb2b8aa597fbb4476b985a021c
Giuseppe Maria Sarda <giuseppe.sarda16@gmail.com>
2025-10-30 17:13:05 +0100

    fix define of AXI_DATA_WIDTH

--- hw/rtl/include/lagd_define.svh
+++ hw/rtl/include/lagd_define.svh
@@ -24,14 +24,14 @@
     `define L2_MEM_BASE_ADDR 'h8000_0000
     `define L2_MEM_ADDR_WIDTH $clog2(`L2_MEM_SIZE_B)
     `define L2_WORDS_PER_BANK 2048
-    `define L2_BANKING_FACTOR `L2_MEM_SIZE_B / (`AXI_DATA_WIDTH/8) / `L2_WORDS_PER_BANK
+    `define L2_BANKING_FACTOR `L2_MEM_SIZE_B / (`LAGD_AXI_DATA_WIDTH/8) / `L2_WORDS_PER_BANK
 
     // CVA6 stack memory
     `define STACK_BASE_ADDR 'h8FFF_8000 // Top of L2 memory minus 32 kB
     `define STACK_SIZE_B 16*1024   // Max 32 kB stack size (according to STACK_BASE_ADDR)
     `define STACK_ADDR_WIDTH $clog2(`STACK_SIZE_B)
     `define STACK_WORDS_PER_BANK 2048
-    `define STACK_BANKING_FACTOR `STACK_SIZE_B / (`AXI_DATA_WIDTH/8) / `STACK_WORDS_PER_BANK
+    `define STACK_BANKING_FACTOR `STACK_SIZE_B / (`LAGD_AXI_DATA_WIDTH/8) / `STACK_WORDS_PER_BANK
 
     // Ising cores
     `define IC_MEM_BASE_ADDR 'h9000_0000
@@ -39,7 +39,7 @@
     // L1 memory per core
     `define IC_L1_MEM_LIMIT 'h10_0000 // 1 MB per core
     `define IC_L1_WORDS_PER_BANK 2048
-    `define IC_L1_BANKING_FACTOR `IC_L1_MEM_SIZE_B / (`AXI_DATA_WIDTH/8) / `IC_L1_WORDS_PER_BANK
+    `define IC_L1_BANKING_FACTOR `IC_L1_MEM_SIZE_B/(`LAGD_AXI_DATA_WIDTH/8)/`IC_L1_WORDS_PER_BANK
     // Registers per core
     `define IC_NUM_REGS 'h1000    // 4 kB per core
 
