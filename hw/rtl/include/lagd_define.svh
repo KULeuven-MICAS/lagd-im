5ae014b9ff3da549e62e65c6b4d7d638174e139c
jiacong sun <jiacong.sun@esat.kuleuven.be>
2025-11-26 16:44:29 +0100

    change J memory data width to 4096

--- hw/rtl/include/lagd_define.svh
+++ hw/rtl/include/lagd_define.svh
@@ -43,7 +43,7 @@
     `define IC_L1_MEM_LIMIT 'h10_0000 // 1 MB per core
     `define IC_L1_WORDS_PER_BANK 2048
     // L1 memory port width
-    `define IC_L1_J_MEM_DATA_WIDTH 256*4
+    `define IC_L1_J_MEM_DATA_WIDTH 4*1024
     `define IC_L1_H_MEM_DATA_WIDTH 4
     `define IC_L1_FLIP_MEM_DATA_WIDTH 256
     // Registers per core
