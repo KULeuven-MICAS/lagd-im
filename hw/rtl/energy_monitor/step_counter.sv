8d3763bbcc1b13da3710d32c6b2596ae91b6790f
jiacong sun <jiacong.sun@esat.kuleuven.be>
2025-11-26 19:29:15 +0100

    fix the counter overflow logic

--- hw/rtl/energy_monitor/step_counter.sv
+++ hw/rtl/energy_monitor/step_counter.sv
@@ -28,7 +28,8 @@
 `include "../lib/registers.svh"
 
 module step_counter #(
-    parameter int COUNTER_BITWIDTH = $clog2(256)
+    parameter int COUNTER_BITWIDTH = $clog2(256),
+    parameter int PARALLELISM = 4
     )(
     input logic clk_i,
     input logic rst_ni,
@@ -38,7 +39,7 @@ module step_counter #(
     input logic recount_en_i,
     input logic step_en_i,
     output logic unsigned [COUNTER_BITWIDTH-1:0] q_o,
-    output logic finish_o
+    output logic overflow_o
 );
     // Internal signals
     logic overflow;
@@ -46,15 +47,15 @@ module step_counter #(
     logic unsigned [COUNTER_BITWIDTH-1:0] counter_reg;
     logic unsigned [COUNTER_BITWIDTH-1:0] counter_n;
 
-    assign counter_n = q_o + 1;
+    assign counter_n = q_o + PARALLELISM;
 
-    assign finish_o = overflow;
+    assign overflow_o = overflow;
 
     // Sequential logic to set the counter target value
     `FFL(counter_reg, d_i, en_i && load_i, {COUNTER_BITWIDTH{1'b1}}, clk_i, rst_ni)
 
     // Sequential logic to update the counter register
     `FFLARNC(q_o, counter_n, en_i && step_en_i && (q_o != counter_reg), en_i && recount_en_i, 'd0, clk_i, rst_ni)
-    `FFLARNC(overflow, 1'b1, en_i && step_en_i && (q_o == counter_reg), en_i && recount_en_i, 'd0, clk_i, rst_ni)
+    `FFLARNC(overflow, 1'b1, en_i && step_en_i && (q_o == (counter_reg - PARALLELISM + 1)), en_i && recount_en_i, 'd0, clk_i, rst_ni)
 
 endmodule
