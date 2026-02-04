0c57df0e5f8bd17d7aafc95667e6892037fe9656
Giuseppe Maria Sarda <giuseppe.sarda16@gmail.com>
2026-02-04 15:42:32 +0100

    extend system level tb to use technology model files

--- ci/sys-run.sh
+++ ci/sys-run.sh
@@ -20,6 +20,7 @@ Usage: ./ci/sys-run.sh [[
     --binary=#binary_path
     --dbg=#dbg_lvl
     --gui
+    --use_tech_models
     --help]]"
 EOF
     echo "Example: $0"
@@ -34,16 +35,27 @@ show_help()
     echo "  --binary=#binary_path: Path to the binary to load into memory (default: helloworld.rom.elf)"
     echo "  --dbg=#dbg_lvl: Debug level (0-3, default: 0)"
     echo "  --gui: Run simulation in GUI mode"
+    echo "  --use_tech_models: Use technology models for the simulation"
     echo "  --help: Show this help message"
 }
 
 SCRIPT_DIR=$(dirname "$0")
 ROOT_DIR=$(realpath "${SCRIPT_DIR}/..")
 
+# Check if pixi is installed (if not we are probably on cygni)
+if ! command -v pixi &> /dev/null; then
+    PIXI=0
+    CHS_PATH=${ROOT_DIR}  # Default dummy val
+else
+    PIXI=1
+    CHS_PATH=$( pixi run bender path cheshire)
+fi
+
 CHIP_LEVEL_TEST=0
 BOOT_MODE=0
 PRELOAD_MODE=0
-PRELOAD_ELF=$( pixi run bender path cheshire)/sw/tests/helloworld.spm.elf
+USE_TECH_MODELS=0
+PRELOAD_ELF=${CHS_PATH}/sw/tests/helloworld.spm.elf
 DBG=0
 NO_GUI=1
 
@@ -77,6 +89,10 @@ for i in "$@"; do
             NO_GUI=0
             shift
             ;;
+        --use_tech_models)
+            USE_TECH_MODELS=1
+            shift
+            ;;
         --help)
             show_help
             exit 0
@@ -101,5 +117,14 @@ echo "  PRELOAD_MODE: $PRELOAD_MODE"
 echo "  PRELOAD_ELF: $PRELOAD_ELF"
 echo "  DBG: $DBG"
 echo "  NO_GUI: $NO_GUI"
+echo "  USE_TECH_MODELS: $USE_TECH_MODELS"
+
+if [ "${PIXI}" -eq 1 ]; then
+    USE_TECH_MODELS=${USE_TECH_MODELS} make -C "${ROOT_DIR}/hw/tb" clean flist
+else
+    echo "[WARNING] Pixi not found, assuming running on cygni. Skipping flist generation."
+    sleep 1
+fi
 CHIP_LEVEL_TEST=${CHIP_LEVEL_TEST} BOOT_MODE=${BOOT_MODE} PRELOAD_MODE=${PRELOAD_MODE} \
-    PRELOAD_ELF=${PRELOAD_ELF} DBG=${DBG} NO_GUI=${NO_GUI} make -C "${ROOT_DIR}/hw/tb" run
+    PRELOAD_ELF=${PRELOAD_ELF} DBG=${DBG} NO_GUI=${NO_GUI} USE_TECH_MODELS=${USE_TECH_MODELS} \
+    make -C "${ROOT_DIR}/hw/tb" run
