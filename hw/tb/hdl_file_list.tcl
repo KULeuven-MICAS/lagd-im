c15356dd8ef89a8faaa67a867562f60efb36c3d6
Giuseppe Maria Sarda <giuseppe.sarda16@gmail.com>
2026-02-16 15:21:47 +0100

    change order of galena_pkg

--- hw/tb/hdl_file_list.tcl
+++ hw/tb/hdl_file_list.tcl
@@ -9,8 +9,8 @@ set HDL_PATH ./
 set HDL_FILES [ list \
     ${HDL_PATH}/tb_lagd_chip.sv \
     ${HDL_PATH}/models/pomelo_pll/pomelo_pll.sv \
-    ${HDL_PATH}/models/galena/galena.sv \
     ${HDL_PATH}/models/galena/galena_pkg.sv \
+    ${HDL_PATH}/models/galena/galena.sv \
 ]
 
 set PROJECT_ROOT [exec realpath ../..]
