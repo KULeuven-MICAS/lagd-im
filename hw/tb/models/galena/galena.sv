a7a33d893b8f07c7e93e064798dfb9dd945b5125
jiacong sun <jiacong.sun@esat.kuleuven.be>
2026-02-24 01:04:33 +0100

    change file path to relative in galena model

--- hw/tb/models/galena/galena.sv
+++ hw/tb/models/galena/galena.sv
@@ -9,11 +9,11 @@
 `timescale 1ns / 1ps
 
 `ifndef STATE_OUT_FILE_1
-`define STATE_OUT_FILE_1 "/users/micas/jsun/sunjc/codes/aria_project/tapeout/lagd-im/sw/tests/data/states_out_1"
+`define STATE_OUT_FILE_1 "../../sw/tests/data/states_out_1" // relative to hw/tb/
 `endif
 
 `ifndef STATE_OUT_FILE_2
-`define STATE_OUT_FILE_2 "/users/micas/jsun/sunjc/codes/aria_project/tapeout/lagd-im/sw/tests/data/states_out_2"
+`define STATE_OUT_FILE_2 "../../sw/tests/data/states_out_2" // relative to hw/tb/
 `endif
 
 import galena_pkg::*;
