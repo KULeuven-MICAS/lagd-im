be73572a0cf36446839ebe98a97ccea2be0c3471
Giuseppe Maria Sarda <giuseppe.sarda16@gmail.com>
2026-03-04 11:50:16 +0100

    fix VCD_GEN_ST_SP call

--- hw/tb/src/tb_lagd_chip.sv
+++ hw/tb/src/tb_lagd_chip.sv
@@ -76,8 +76,7 @@ module tb_lagd_chip ();
   // Add vcd dumping and debug setup
   //==============================================
   if (ChipTest == 1) begin : gen_debug_setup
-    `VCD_GEN_ST_SP(`VCD_DUMP, `VCD_FILE, tb_lagd_chip.fix.gen_dut_lagd_chip.dut, \
-      enable_vcd_dumping, `VCD_STOP, `END_SIM_AT_VCD_STOP)
+    `VCD_GEN_ST_SP(`VCD_DUMP, `VCD_FILE, tb_lagd_chip.fix.gen_dut_lagd_chip.dut, `VCD_START, `VCD_STOP, `END_SIM_AT_VCD_STOP)
   end
 
 endmodule
